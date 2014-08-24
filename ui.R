5dd7a9faa59fccd84ea068aa0e69f1183d298421
mliq <mliq@users.noreply.github.com>
2014-08-24 13:09:41 -0500

    Broken

--- ui.R
+++ ui.R
@@ -2,25 +2,28 @@ shinyUI(fluidPage(
   title="Health Expenditure Per Capita (Current US$)", 
   h2("Health Expenditure Per Capita (Current US$)"),
   h6("Source:", {a("World Bank", href="http://data.worldbank.org/indicator/SH.XPD.PCAP/countries?display=default")}),
-  htmlOutput('myMap'),
-  h4("Legend:", "100", {img(src="legend.png")}, "4000"),
-  hr(),
   fluidRow(
-    column(2,
+    column(4,
+           htmlOutput('myMap'),
+           h4("Legend:", "100", {img(src="legend.png")}, "4000")
+      ),
+    column(8,
+             h4("Data Table (sortable)"),
+             htmlOutput('myTable')
+      )),
+#  hr(),
+  fluidRow(
+    column(4,
            h4("Map Year"),           
            sliderInput('year', 'Year',format ="####",  min=2010, max=2012, value=2012, 
                        step=1),
-           br()
+           #br()
     ),
-    column(10, 
+    column(8, 
            h4("Calculate Annual Growth Rate:"),
            uiOutput("cSelector"),
            tableOutput('myTable2'),
            h4(htmlOutput('text1'))
     )
-  ),
-  fluidRow(
-    h4("Data Table (sortable)"),
-    htmlOutput('myTable')
-    )
+  )
 ))
