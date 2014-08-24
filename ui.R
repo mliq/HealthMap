35b493fed87b20d33d675563a3091cdcf2e0b66d
mliq <mliq@users.noreply.github.com>
2014-08-24 13:27:02 -0500

    Layout Changes

--- ui.R
+++ ui.R
@@ -2,25 +2,29 @@ shinyUI(fluidPage(
   title="Health Expenditure Per Capita (Current US$)", 
   h2("Health Expenditure Per Capita (Current US$)"),
   h6("Source:", {a("World Bank", href="http://data.worldbank.org/indicator/SH.XPD.PCAP/countries?display=default")}),
-  htmlOutput('myMap'),
-  h4("Legend:", "100", {img(src="legend.png")}, "4000"),
+  #htmlOutput('myMap'),
+  #h4("Legend:", "100", {img(src="legend.png")}, "4000"),
   hr(),
+  htmlOutput('myMap'),
   fluidRow(
-    column(2,
-           h4("Map Year"),           
-           sliderInput('year', 'Year',format ="####",  min=2010, max=2012, value=2012, 
-                       step=1),
-           br()
+    column(3,
+           sliderInput('year', 'Map Year',format ="####",  min=2010, max=2012, value=2012, 
+                       step=1)
     ),
-    column(10, 
-           h4("Calculate Annual Growth Rate:"),
+    column(4,
+           h5("Legend:", "100", {img(src="legend.png")}, "4000")
+           )
+  ),
+  fluidRow(
+    column(4, 
+           h4("Calculate Growth Rates"),
            uiOutput("cSelector"),
            tableOutput('myTable2'),
            h4(htmlOutput('text1'))
+    ),
+    column(4,
+           h4("Data Table (sortable)"),
+           htmlOutput('myTable')
     )
-  ),
-  fluidRow(
-    h4("Data Table (sortable)"),
-    htmlOutput('myTable')
-    )
+  )
 ))
